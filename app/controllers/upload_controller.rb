b73da9a7c5f4fcb08459228c3d173ea8ecda185e
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2014-10-04 22:43:37 +0900

    change dir name 'upload' as 'beuto-bin'

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -2,23 +2,25 @@
 class UploadController < ApplicationController
   skip_before_filter :authorize, only: [:new]
 
-  if Rails.env == "development"
-    DESTDIR = File.join(Rails.root,"upload")
-  else
-    DESTDIR = "/srv/robocar/upload"
-  end
+  # if Rails.env == "development"
+  #   DESTDIR = File.join(Rails.root,"binaries")
+  # else
+  #   DESTDIR = "/srv/robocar-2014/binaries"
+  # end
+
+  DESTDIR = File.join(Rails.root, "beuto-bin")
 
   def index
   end
 
   def upload
-    gid = Student.find_by_id(session[:student_id]).group_id
+    gid = (Student.find_by_id(session[:student_id]).group_id or
+           "unknown")
     if params[:bin].nil? or params[:bin][:name].nil?
       flash[:alert] = "送信失敗：ファイルを選んでいません。"
     elsif params[:bin][:name].original_filename !~ /\.bin\Z/
       flash[:alert] = "送信失敗：バイナリファイルではありません。"
     else
-#      name = "#{gid}_#{Time.now.to_i % 10000}.bin"
       name = "#{gid}.bin"
       File.open(File.join(DESTDIR, name),"wb") do |f|
         f.write params[:bin][:name].read
