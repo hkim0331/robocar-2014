04e327223503db961870eff97b2af2944fc943de
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2014-10-08 21:22:45 +0900

    forgot commit.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -18,7 +19,6 @@ File.foreach("data/sid_uid.txt") do |line|
 end
 puts "\nadd #{s} students."
 
-puts "seed Icome"
 Icome.delete_all
 i = 0
 File.foreach("data/ucome.log") do |line|
@@ -30,26 +30,26 @@ File.foreach("data/ucome.log") do |line|
 end
 puts "\nadd #{i} icome entries."
 
-puts "seed Car"
 Car.delete_all
 Car::Max.times do
   Car.create(condition: Car::Free);
   puts '.'
 end
 
-puts "\nseed Week"
 Week.delete_all
 (1..16).each do |w|
   Week.create(iteration: w)
   print '.'
 end
 
-puts "\nseed Lent"
 Lent.delete_all
 (1..Car::Max).each do |c|
 	Lent.create(car_id: c, condition: Car::Free, sid: 'hkimura', contact: '3457')
   print '.'
 end
+
+Group.delete_all
+Group.create(name: "2014宇宙の旅", members: "hkimura yoshino")
 
 puts "\ndone"
 
